586f312f40854d9d7696637372ced8b4087c6dbc
akihirohisada <ah.nt823@gmail.com>
2019-08-22 07:44:59 +0900

    post menu

--- routes/api/menu.js
+++ routes/api/menu.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/menu
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Menu route'));
+const Menu = require('../../models/Menu');
+const User = require('../../models/User');
+
+// @route    POST api/menu
+// @desc     Create a menu
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'メニュー名をご記入ください。')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newMenu = new Menu({
+        title: req.body.title,
+        name: user.name,
+        user: req.user.id
+      });
+
+      const menu = await newMenu.save();
+
+      res.json(menu);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
